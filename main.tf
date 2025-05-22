88bebd0e03f015fa93321337dcd61dfa7f4705b2
kapilrajpoot8700 <90081401+kapilrajpoot8700@users.noreply.github.com>
2025-05-22 17:11:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "my_ec2" {
   source = "./modules/ec2"
 }
-
 module "my_s3" {
   source = "./modules/s3"
 }
 
 output "test_vm_public_ip" {
-  value = module.my_ec2.a1
+  value = module.my_ec2.public_ip
+}
+output "test_vm_private_ip" {
+  value = module.my_ec2.private_ip
 }
 
-output "test_vm_private_ip" {
-  value = module.my_ec2.a2
+output "test_vm_bucket_id" {
+  value = module.my_s3.s3_bucket_id
 }
